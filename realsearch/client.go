a5b3fa97ade30d4d1bc51e0f2604cb3b9cd66a61
wifufu <205970380+wifufu@users.noreply.github.com>
2025-05-05 01:15:24 +0800

    feat(real search): client request factory

--- realsearch/client.go
+++ realsearch/client.go
@@ -1,6 +1,9 @@
 package realsearch
 
 import (
+	"encoding/json"
+	"fmt"
+	"io"
 	"net/http"
 	"net/http/httputil"
 	"net/url"
@@ -40,4 +43,43 @@ func (c Client) NewProxy() *Proxy {
 	return &Proxy{
 		httputil.NewSingleHostReverseProxy(u),
 	}
+}
+
+type ResponseSuccess struct {
+	Data interface{} `json:"data"`
+}
+
+type ResponseError struct {
+	Code uint8  `json:"code"`
+	Msg  string `json:"msg"`
+}
+
+func (resp ResponseError) Error() string {
+	return fmt.Sprintf("real search api error: code: %d, msg: %s", resp.Code, resp.Msg)
+}
+
+func (c Client) NewRequest(method, path string, body io.Reader) (*http.Request, error) {
+	return http.NewRequest(method, c.baseUrl+path, body)
+}
+
+func (c Client) Do(req *http.Request, data interface{}) error {
+	httpRes, err := c.httpClient.Do(req)
+	if err != nil {
+		return err
+	}
+	defer httpRes.Body.Close()
+
+	if httpRes.StatusCode > 299 {
+		var errResp ResponseError
+		err := json.NewDecoder(httpRes.Body).Decode(&errResp)
+		if err == nil {
+			return &errResp
+		}
+		return fmt.Errorf("real search api internal error with httpstatus %d", httpRes.StatusCode)
+	}
+
+	var res = ResponseSuccess{
+		Data: data,
+	}
+	return json.NewDecoder(httpRes.Body).Decode(&res)
 }
