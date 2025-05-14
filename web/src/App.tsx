458afce65db5eb7077dbc18d0e8eec075fda7839
wifufu <205970380+wifufu@users.noreply.github.com>
2025-05-15 00:01:26 +0800

    improve(web): app shell header layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import { createTheme, MantineProvider } from "@mantine/core";
 const theme = createTheme({});
 
 import Picture from "@component/Picture.tsx";
-import { AppShell, Burger, Flex, Title } from "@mantine/core";
+import { AppShell, Burger, Group, Title, Badge, NavLink } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 
 export const App: FC = () => {
@@ -24,33 +24,33 @@ export const App: FC = () => {
         padding="md"
       >
         <AppShell.Header>
-          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
-          <Flex
-            align={"center"}
-            style={{
-              height: "100%",
-            }}
-          >
-            <Picture
-              name={"logo"}
-              alt={"logo"}
-              imgStyle={{
-                height: "2.1rem",
-                marginLeft: "1.2rem",
-                marginRight: "0.4rem",
-                aspectRatio: 1,
-              }}
+          <Group h="100%" px="md">
+            <Burger
+              opened={opened}
+              onClick={toggle}
+              hiddenFrom="sm"
+              size="sm"
             />
-            <Title
-              order={3}
-              style={{
-                letterSpacing: 1,
-                fontWeight: "bolder",
-              }}
-            >
-              ONEST
-            </Title>
-          </Flex>
+            <Group h="100%" gap="sm">
+              <Picture
+                name={"logo"}
+                alt={"logo"}
+                imgStyle={{
+                  height: "2.1rem",
+                  aspectRatio: 1,
+                }}
+              />
+              <Title
+                order={3}
+                style={{
+                  letterSpacing: 1,
+                  fontWeight: "bolder",
+                }}
+              >
+                ONEST
+              </Title>
+            </Group>
+          </Group>
         </AppShell.Header>
 
         <AppShell.Navbar p="md">Navbar</AppShell.Navbar>
