5ac7efb62aacdc154b74b5158f38b243e13d9689
wifufu <205970380+wifufu@users.noreply.github.com>
2025-05-14 19:05:18 +0800

    build(web): setup mantine

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,13 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode></StrictMode>,
-)
+import "@mantine/core/styles.css";
+import { createTheme, MantineProvider } from "@mantine/core";
+
+const theme = createTheme({});
+
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <MantineProvider theme={theme}></MantineProvider>
+  </StrictMode>,
+);
