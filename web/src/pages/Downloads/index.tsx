27e614283e538a80e623fe69c9ba54d9368919ef
wifufu <205970380+wifufu@users.noreply.github.com>
2025-05-20 03:53:10 +0800

    fix(web): requesting download tasks

--- web/src/pages/Downloads/index.tsx
+++ web/src/pages/Downloads/index.tsx
@@ -1,15 +1,21 @@
 import { type FC } from "react";
-import useSWR from "swr";
 
 import { Flex, Card, Text, Badge } from "@mantine/core";
 
+import useSWR from "swr";
+import api from "@network/api.ts";
+
 export const Downloads: FC = () => {
-  const { data: tasks } = useSWR<Download.Task[]>("download/task", {
-    revalidateOnFocus: true,
-    refreshInterval: 3000,
-    refreshWhenHidden: false,
-    refreshWhenOffline: true,
-  });
+  const { data: tasks } = useSWR<Download.Task[]>(
+    "download/tasks",
+    (url: string) => api.get(url).then((res) => res.data.data),
+    {
+      revalidateOnFocus: true,
+      refreshInterval: 3000,
+      refreshWhenHidden: false,
+      refreshWhenOffline: true,
+    },
+  );
 
   return (
     <>
